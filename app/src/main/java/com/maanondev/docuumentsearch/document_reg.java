032934ec023833b2ebd2ab4d25fc522c2095c6ac
Pakiya <pk.kumar7500@gmail.com>
2017-09-14 16:22:34 +0530

    Initial commit

---
+++ app/src/main/java/com/maanondev/docuumentsearch/document_reg.java
@@ -0,0 +1,13 @@
+package com.maanondev.docuumentsearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class document_reg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document_reg);
+    }
+}
