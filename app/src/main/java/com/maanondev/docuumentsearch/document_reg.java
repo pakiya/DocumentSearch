ae841bf1974a3635bf1ee1a0b0819b8b6076330b
Pankaj kumar <pk.kumar7500@gmail.com>
2017-09-19 21:35:07 +0530

    Add files via upload

--- app/src/main/java/com/maanondev/docuumentsearch/document_reg.java
+++ app/src/main/java/com/maanondev/docuumentsearch/document_reg.java
@@ -2,6 +2,12 @@ package com.maanondev.docuumentsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class document_reg extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class document_reg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_document_reg);
+
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        final DatabaseReference myRef=database.getReferenceFromUrl("https://docuumentsearch.firebaseio.com/document_reg/");
+
+
+        final EditText holder_name= (EditText) findViewById(R.id.holder_name);
+        final EditText holder_fname= (EditText) findViewById(R.id.holder_fname);
+        final EditText holder_dob= (EditText) findViewById(R.id.holder_dob);
+        final EditText finder_name= (EditText) findViewById(R.id.finder_name);
+        final EditText mobile_no= (EditText) findViewById(R.id.mobile_no);
+        Button submit_d= (Button) findViewById(R.id.submit_d);
+
+        submit_d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.child(mobile_no.getText().toString()).child("Holder's Name").setValue(holder_name.getText().toString());
+                myRef.child(mobile_no.getText().toString()).child("Holder's Fname").setValue(holder_fname.getText().toString());
+                myRef.child(mobile_no.getText().toString()).child("Holder's DOB").setValue(holder_dob.getText().toString());
+                myRef.child(mobile_no.getText().toString()).child("Finder's Name").setValue(finder_name.getText().toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/maanondev/docuumentsearch/find_document.java
+++ app/src/main/java/com/maanondev/docuumentsearch/find_document.java
@@ -2,6 +2,16 @@ package com.maanondev.docuumentsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class find_document extends AppCompatActivity {
 
@@ -9,5 +19,37 @@ public class find_document extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_document);
+
+        final FirebaseDatabase Database=FirebaseDatabase.getInstance();
+        DatabaseReference myRef=Database.getReferenceFromUrl("https://docuumentsearch.firebaseio.com/document_reg/");
+
+        EditText your_name= (EditText) findViewById(R.id.your_name);
+        EditText father_name= (EditText) findViewById(R.id.father_name);
+        EditText dob= (EditText) findViewById(R.id.dob);
+
+        TextView out_put= (TextView) findViewById(R.id.out_put);
+
+        Button search= (Button) findViewById(R.id.search);
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Database.getReference("document_reg").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
     }
 }
